f56568a179177235f29f33efe57947c47ebde7ef
milkakeza <milkakeza9@gmail.com>
2025-11-09 23:57:41 +0200

    Refactor codebase to resolve Dart analyzer warnings for the post_book screen

--- lib/screens/post_book_screen.dart
+++ lib/screens/post_book_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use, use_build_context_synchronously, curly_braces_in_flow_control_structures
+
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
